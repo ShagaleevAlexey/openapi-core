6b2d8e707f5a10209c2447a05111428d30b718b5
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-08-24 19:04:56 +0500

    bump version

--- openapi_core/__init__.py
+++ openapi_core/__init__.py
@@ -4,10 +4,10 @@ from openapi_core.shortcuts import (
     create_spec, validate_parameters, validate_body, validate_data,
 )
 
-__author__ = 'Artur Maciag'
-__email__ = 'maciag.artur@gmail.com'
-__version__ = '0.11.0'
-__url__ = 'https://github.com/p1c2u/openapi-core'
+__author__ = 'Shagaleev Alexey'
+__email__ = 'alexey.shagaleev@yandex.ru'
+__version__ = '0.12.0'
+__url__ = 'https://github.com/ShagaleevAlexey/openapi-core'
 __license__ = 'BSD 3-Clause License'
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ metadata = get_metadata(init_py)
 
 
 setup(
-    name='openapi-core',
+    name='fixed-openapi-core',
     version=metadata['version'],
     author=metadata['author'],
     author_email=metadata['email'],
